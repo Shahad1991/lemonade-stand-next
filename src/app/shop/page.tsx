706eb0b02bffbc48c6783572e15ca3ad6fca6cb2
Shahad Al_ jawashee <shahad1nazar@gmail.com>
2025-03-27 10:53:43 +0100

    fix errors to deploy

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -7,8 +7,7 @@ interface Cocktail {
   idDrink: string;
   strDrink: string;
   strDrinkThumb: string;
-  description: string;
-  price: number;
+  
 }
 
 export default function ShopPage() {
@@ -33,9 +32,10 @@ export default function ShopPage() {
           console.error("Unexpected response format:", data);
           throw new Error("Unexpected response format");
         }
-      } catch (error) {
-        console.error("Failed to fetch cocktails:", error);
-        setError(error.message);
+      } catch (err) {
+        const errorMessage = err instanceof Error ? err.message : "An unknown error occurred";
+        console.error("Failed to fetch cocktails:", errorMessage);
+        setError(errorMessage);
       } finally {
         setLoading(false);
       }
@@ -54,12 +54,15 @@ export default function ShopPage() {
 
   return (
     <div className="container m-auto  p-4">
-     
       <div className="space-y-4 mt-4">
         {cocktails.map((cocktail) => (
           <div key={cocktail.idDrink} className="flex justify-between items-center border p-4 rounded-lg">
             <div className="flex items-center">
-              <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} className="w-16 h-16 object-cover rounded-lg mr-4" />
+              <img
+                src={cocktail.strDrinkThumb}
+                alt={cocktail.strDrink}
+                className="w-16 h-16 object-cover rounded-lg mr-4"
+              />
               <span className="font-semibold">{cocktail.strDrink}</span>
             </div>
             <span> Price: $10</span>
@@ -69,13 +72,11 @@ export default function ShopPage() {
                 setClicked((prevState) => ({ ...prevState, [cocktail.idDrink]: true }));
               }}
               className={`py-2 px-4 rounded text-white ${
-                clicked[cocktail.idDrink] ? "bg-green-500" : "bg-green-800"}
+                clicked[cocktail.idDrink] ? "bg-green-500" : "bg-green-800"
               }`}
             >
               {clicked[cocktail.idDrink] ? "Added to Cart" : "Add to Cart"}
             </button>
-            
-            
           </div>
         ))}
       </div>
